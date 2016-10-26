e0be9d59f260d9deab99d2df8e63238a6f79ab4d
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-10-25 21:28:01 -0400

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,16 @@
+const counterStore = require('../stores/counterStore.js');
+
+function increment() {
+  counterStore.increment();
+}
+
+function decrement() {
+  counterStore.decrement();
+}
+
 module.exports = {
   // Your implementation here.
   // Hint: We need two actions!
+  increment,
+  decrement
 };
