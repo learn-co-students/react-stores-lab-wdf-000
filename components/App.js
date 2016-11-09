fbf1be9715975f1d788a907d4ae0af7d7fb45480
satub <satu.barnhill@gmail.com>
2016-11-09 15:59:35 -0500

    Done

--- components/App.js
+++ components/App.js
@@ -1,27 +1,46 @@
 const React = require('react');
+const counterStore = require('../stores/counterStore');
+const actions = require('../actions')
 
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      // Your implementation here.
+      counter: counterStore.getState()
     };
+    this.add = this.add.bind(this);
+    this.remove = this.remove.bind(this);
   }
   componentDidMount () {
     // Your implementation here.
+    this.removeListener = counterStore.addListener(counter => {
+      this.setState({ counter })
+    });
   }
+
   componentWillUnmount () {
-    // Your implementation here.
+    this.removeListener();
   }
+
+  add(ev){
+    ev.preventDefault();
+    actions.increment();
+  }
+  remove(ev){
+    ev.preventDefault();
+    actions.decrement();
+  }
+
   render () {
     return (
       <div className='app'>
-        <h1 className='counter'></h1>
+        <h3 className='iDoDeclare'>{this.state.counter == 8 ? 'WOOHOO! INFINITY, LOPSIDED!' : ''}</h3>
+        <h1 className='counter'>{this.state.counter}</h1>
         <div className='actions'>
-          <button className='decrement'>
+          <button className='decrement' onClick={this.remove}>
             -
           </button>
-          <button className='increment'>
+          <button className='increment' onClick={this.add}>
             +
           </button>
         </div>
--- stores/Store.js
+++ stores/Store.js
@@ -2,6 +2,29 @@ class Store {
   // Your implementation here.
   // Hint: Our <App /> component won't directly use this store, but instead the
   // CounterStore is going to inherit from it.
+  constructor(initialState){
+    this.state = initialState;
+    this.listeners = [];
+  }
+
+  setState(status){
+    this.state = status;
+      for (let listener of this.listeners) {
+        listener.call(this, status);
+      }
+  }
+
+  getState(){
+    return this.state;
+  }
+
+  addListener(listener){
+    this.listeners.push(listener);
+    const removeListener = () => {
+      this.listeners = this.listeners.filter((listn) => listener !== listn);
+    };
+    return removeListener;
+  }
 }
 
 module.exports = Store;
