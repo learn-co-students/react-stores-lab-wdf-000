1290289ac591fa99b4459c1e5fe698d29413afbe
blessedsoy <unmigong@gmail.com>
2017-02-22 14:05:41 -0500

    Done.

--- components/App.js
+++ components/App.js
@@ -1,27 +1,47 @@
 const React = require('react');
+const actions = require('../actions');
+const counterStore = require('../stores/counterStore')
 
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
       // Your implementation here.
+        counter: counterStore.getState()
     };
+    this.handleIncrementClick = this.handleIncrementClick.bind(this);
+    this.handleDecrementClick = this.handleDecrementClick.bind(this);
   }
+
   componentDidMount () {
+    this.removeListener = counterStore.addListener(state => {
+      this.setState({counter: state});
+    });
     // Your implementation here.
   }
   componentWillUnmount () {
+    this.removeListener()
     // Your implementation here.
   }
+
+  handleIncrementClick (ev) {
+    ev.preventDefault()
+    actions.increment()
+  }
+  handleDecrementClick (ev) {
+    ev.preventDefault()
+    actions.decrement()
+  }
+
   render () {
     return (
       <div className='app'>
-        <h1 className='counter'></h1>
+        <h1 className='counter'>{this.state.counter}</h1>
         <div className='actions'>
-          <button className='decrement'>
+          <button className='decrement' onClick={this.handleDecrementClick} >
             -
           </button>
-          <button className='increment'>
+          <button className='increment' onClick={this.handleIncrementClick} >
             +
           </button>
         </div>
@@ -29,5 +49,6 @@ class App extends React.Component {
     );
   }
 }
+
 
 module.exports = App;
--- stores/counterStore.js
+++ stores/counterStore.js
@@ -1,6 +1,22 @@
-class CounterStore {
+const Store = require('./Store');
+
+class CounterStore extends Store {
   // Your implementation here.
   // Hint: extend the Store class!
+  constructor () {
+    super(0);
+  }
+
+  increment(){
+    const increased = this.getState() + 1
+    return this.setState(increased)
+  }
+
+  decrement(){
+    const decreased = this.getState() - 1
+    return this.setState(decreased)
+  }
+
 }
 
 module.exports = new CounterStore();
